d40ef55ab46fdfaa16cf618fc098776c2fcfbbc0
Wilfredo Abregu <fabregu01@gmail.com>
2025-03-11 22:46:40 -0500

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Product } from './product.model'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'plazy-ng';
+  items = ['Juan', 'Julio', 'Ada'];
+
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/image/foto.jpg',
+      title: 'Foto 1',
+      price: 14.00,
+      description: 'XD'
+    },
+    {
+      id: '2',
+      image: 'assets/image/foto2.jpg',
+      title: 'Foto 2',
+      price: 15.00,
+      description: 'XD'
+    },
+    {
+      id: '3',
+      image: 'assets/image/foto3.jpg',
+      title: 'Foto 3',
+      price: 29.00,
+      description: 'XD'
+    }
+  ];
+
+  addItem() {
+    this.items.push('nuevo item');
+  }
+
+  deleteItem(index: number) {
+    this.items.splice(index, 1);
+  }
 }
